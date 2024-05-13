21d8c24281775926ece08a976228a8e10e4b5141
Monica Moreno <169068483+MonicaMoren@users.noreply.github.com>
2024-05-13 19:21:35 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+      public static void main(String[] args) {
+        System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+        System.out.println("********************");
+        System.out.println("*Filme : Divergente*");
+        System.out.println("********************");
+        System.out.println(" ");
+        System.out.println("Lançamento do Filme Divergente");
+        System.out.println("17 de Abril de 2014 ");
+        System.out.println(" ");
+        System.out.println("Sinopse ");
+
+        System.out.println("==============================================================");
+
+        System.out.println("Na futurística cidade de Chicago, ao completar 16 anos, Beatrice precisa escolher entre as diferentes facções em que a cidade está dividida. Cada uma representa um valor diferente e, ao contrário de sua família, a jovem opta pela facção dos destemidos, a Audácia. Ela então se torna Tris e inicia uma jornada para afastar seus medos e descobrir quem realmente é. Durante essa jornada, acaba conhecendo o jovem Quatro, um rapaz experiente que tem o dom de intrigá-la e de encantá-la ao mesmo tempo.");
+        System.out.println("============================================================== ");
+        System.out.println(" ");
+        System.out.println("+++++++++++++++++++++++");
+        System.out.println("+ Endereço do Trailer +");
+        System.out.println("+++++++++++++++++++++++");
+        System.out.println("  ");
+
+        System.out.println("https://www.youtube.com/watch?v=ALIaNcHNcUs");
+        System.out.println(" ");
+        System.out.println("Opinião: Muito Bom ");
+        System.out.println(" ");
+      
+        System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+
+  }
+}
